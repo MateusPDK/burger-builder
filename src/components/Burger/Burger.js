a4c6d9f58cb875a17881f5f31e0031ff53b67317
unknown <mateus@agenciagh.com.br>
2019-01-09 20:24:12 -0200

    Funcões de adicionar e remover ingredientes funcionando.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styles from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []); // Esse código transforma as 4 arrays em uma unica array contendo todos os valores
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding some ingredients!</p>
+    }
+
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bot" />
+        </div>
+    );
+};
+
+export default burger;
